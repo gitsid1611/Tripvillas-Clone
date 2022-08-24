ca0334a5acc03e46e77d6b0ef062c62b136fa260
furqan5921 <101380683+furqan5921@users.noreply.github.com>
2022-08-24 22:06:37 +0530

    done navbar

---
+++ components/navbar.js
@@ -0,0 +1,11 @@
+function navbar(){
+    return `  <div><img src="https://d2v8elt324ukrb.cloudfront.net/static/riotuikit/images/logo.c72056a22f91.png" alt=""></div>
+    <div>
+        <svg width="35" height="22" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" data-svg="user"><circle fill="none" stroke="rgb(255,255,255)" stroke-width="1.1" cx="9.9" cy="6.4" r="4.4"></circle><path fill="none" stroke="rgb(255,255,255)" stroke-width="1.1" d="M1.5,19 C2.3,14.5 5.8,11.2 10,11.2 C14.2,11.2 17.7,14.6 18.5,19.2"></path></svg>
+        <span>INR</span>
+        <svg width="20" height="15" stroke="rgb(255,255,255)" fill="white" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" data-svg="triangle-down"><polygon points="5 7 15 7 10 12"></polygon></svg>
+        <svg width="20" height="20" stroke="rgb(255,255,255)" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" data-svg="navbar-toggle-icon"><rect y="9" width="20" height="2"></rect><rect y="3" width="20" height="2"></rect><rect y="15" width="20" height="2"></rect></svg>
+    </div>`
+}
+
+export default navbar;
